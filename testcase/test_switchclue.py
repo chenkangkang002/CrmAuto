4bb603d6aa00052ba4218c6e1cd10745f905de21
kitty <535889071@qq.com>
2020-07-24 17:11:16 +0800

    修改

--- testcase/test_switchclue.py
+++ testcase/test_switchclue.py
@@ -8,13 +8,11 @@ from testcase.base_test import BaseTest
 class SwitchClueTestCase(BaseTest):
     def test_clue(self):
         '''线索转换为客户'''
-        login = LoginPage(self.driver)  # 实例化LoginPage类
-        u_name = 'admin'
-        password = 'admin123'
-        login.login(u_name, password)  # 调用login page的login方法
+
         addclue = AddCluePage(self.driver)  # 实例化AddCluePage类
         addclue.ele_clickclue()  # 调用AddCluePage的ele_clickclue方法
-        switch = SwitchCluePage(self.driver)
+
+        switch = SwitchCluePage(self.driver)  # 实例化SwitchCluePage
         customer = '李白白'
         text = switch.ele_switchclue(customer)  # 调用clue_page的ele_switchclue方法
         self.assertIn("添加客户成功", text)
